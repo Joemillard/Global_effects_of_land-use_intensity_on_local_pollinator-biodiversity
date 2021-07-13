8e707bb56c020b002ea1c27b1a5cefae50f9cc0e
joemillard <joeymillard@hotmail.co.uk>
2021-07-13 15:13:47 +0100

    amend file paths for predicts compilation

--- R/01_PREDICTS_compilation.R
+++ R/01_PREDICTS_compilation.R
@@ -4,12 +4,12 @@ library(dplyr)
 ### read in the pollinator data and PREDICTS
 
 # read in direct confidence dataframe and filter for Y
-direct_confidence <- read.csv("~/PhD/Aims/Aim 2 - understand response to environmental change/Data/global clade extrapolation/global_polllinating_confirmation_manual-edit.csv", stringsAsFactors = FALSE)
+direct_confidence <- read.csv("C:/Users/joeym/Documents/PhD/Aims/Aim 2 - understand response to environmental change/Data/global clade extrapolation/global_polllinating_confirmation_manual-edit.csv", stringsAsFactors = FALSE)
 direct_confidence <- direct_confidence %>%
   filter(Pollinating.evidence.reference == "Y")
 
 # read in clade extrapolation dataframe, filter out subfamily and tribe, and seperate out genera into different dataframe
-clade_extrapolation <- read.csv("~/PhD/Aims/Aim 2 - understand response to environmental change/Data/global clade extrapolation/clade_extrapolation_2.csv", stringsAsFactors = FALSE)
+clade_extrapolation <- read.csv("C:/Users/joeym/Documents/PhD/Aims/Aim 2 - understand response to environmental change/Data/global clade extrapolation/clade_extrapolation_2.csv", stringsAsFactors = FALSE)
 
 # seperate genera from extrapolation
 clade_extrapolation_genera <- clade_extrapolation %>%
@@ -22,7 +22,7 @@ clade_extrapolation <- clade_extrapolation %>%
   filter(clade_rank != "genus")
 
 # read in clade extrapolation non text-analysis
-extrap_non_text_analysis <- read.csv("~/PhD/Aims/Aim 2 - understand response to environmental change/Data/global clade extrapolation/clade_extrapolation_non_text-analysis.csv", stringsAsFactors = FALSE)
+extrap_non_text_analysis <- read.csv("C:/Users/joeym/Documents/PhD/Aims/Aim 2 - understand response to environmental change/Data/global clade extrapolation/clade_extrapolation_non_text-analysis.csv", stringsAsFactors = FALSE)
 
 # seperate genera from extrapolation
 extrap_non_text_analysis_genera <- extrap_non_text_analysis %>%
@@ -33,10 +33,10 @@ extrap_non_text_analysis <- extrap_non_text_analysis %>%
   filter(clade_rank == "family")
 
 # read in non-family/tribe genus list
-non_family_genus_list <- read.csv("~/PhD/Aims/Aim 2 - understand response to environmental change/Data/global clade extrapolation/non_family-genus_species-list.csv", stringsAsFactors = FALSE)
+non_family_genus_list <- read.csv("C:/Users/joeym/Documents/PhD/Aims/Aim 2 - understand response to environmental change/Data/global clade extrapolation/non_family-genus_species-list.csv", stringsAsFactors = FALSE)
 
 # read in the PREDICTS data, and create extra copy just with taxonomic data for size
-PREDICTS <- readRDS("Data/PREDICTS/database.rds")
+PREDICTS <- readRDS("C:/Users/joeym/Documents/PhD/Aims/Aim 2 - understand response to environmental change/Data/PREDICTS/database.rds")
 PREDICTS_taxa <- PREDICTS %>%
   select(Kingdom, Phylum, Class, Order, Family, Genus) %>%
   filter(Kingdom == "Animalia") %>%
